83fc4606722e55c0b9d3470060a7e8c0c54b3670
DCI-REC102 <otakulp@gmx.de>
2019-12-10 14:13:46 +0100

    public build, update title & favi

---
+++ build/precache-manifest.966405b22ebfae2a98dc08095a4017e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "006aa0ef06c69587b313b09282f902aa",
+    "url": "/pkmn-app/index.html"
+  },
+  {
+    "revision": "bbd19ccb5797676a2d78",
+    "url": "/pkmn-app/static/css/main.6b5a1168.chunk.css"
+  },
+  {
+    "revision": "a391643820216ddbd76c",
+    "url": "/pkmn-app/static/js/2.0fc75db7.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/pkmn-app/static/js/2.0fc75db7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bbd19ccb5797676a2d78",
+    "url": "/pkmn-app/static/js/main.cf90e0df.chunk.js"
+  },
+  {
+    "revision": "93d5975c406002f14d9c",
+    "url": "/pkmn-app/static/js/runtime-main.911fd800.js"
+  }
+]);
